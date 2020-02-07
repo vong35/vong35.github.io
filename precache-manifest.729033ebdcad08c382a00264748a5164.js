89595800c975f1c5eadf5f9325af46fb80ffae93
DAH 01 <dah01@FORMATION.lan>
2020-02-07 11:32:43 +0100

    premier commit

---
+++ precache-manifest.729033ebdcad08c382a00264748a5164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498e42ca95a0d22f05da73fd9a6201b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6258b9867c14e8cf3b41",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6894155213dee8ff686e",
+    "url": "/static/js/2.9b8489fa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9b8489fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6258b9867c14e8cf3b41",
+    "url": "/static/js/main.9ea183c3.chunk.js"
+  },
+  {
+    "revision": "d120168167073cb92b27",
+    "url": "/static/js/runtime-main.9478fcbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
